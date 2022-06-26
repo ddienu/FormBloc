2440b504707979c77947c99533644f0ec845e0ea
ddienu <rockdann@hotmail.com>
2022-06-25 22:14:49 -0500

    Add: Provider page, update: main
    Create provider page with the class and the super class Inherited Widget.
    In the main page update the material app with the provider with its child.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formulariosbloc/src/bloc/provider.dart';
 import 'package:formulariosbloc/src/pages/home_page.dart';
 
 import 'package:formulariosbloc/src/pages/login_page.dart';
@@ -6,9 +7,14 @@ import 'package:formulariosbloc/src/pages/login_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+   
+   
+    return Provider(
+      child: MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       initialRoute: 'login',
@@ -16,6 +22,7 @@ class MyApp extends StatelessWidget {
         'login' : (BuildContext context) => LoginPage(),
         'home'  : (BuildContext context) => HomePage(),
       },
-    );
-  }
+    )
+   );
+ }
 }
---
+++ lib/src/bloc/provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:formulariosbloc/src/bloc/login_bloc.dart';
+
+
+class Provider extends InheritedWidget {
+
+  final loginBloc = LoginBloc();
+
+  Provider({Key? key, required super.child}) : super(key: key);
+  
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+  static LoginBloc? of (BuildContext context ) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>()?.loginBloc;
+  }
+  }
+
