588e27e3625cd74a9fc8badfa29f66c01db663e3
ddienu <rockdann@hotmail.com>
2022-06-28 21:56:17 -0500

    Update: Provider page.
    Create a static variable to mantain the data of the stream even when a hot restart happends.

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -7,9 +7,21 @@ export 'package:formulariosbloc/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
 
+  static Provider? _instancia;
+
+  factory Provider ({Key? key, required Widget child}){
+    if (_instancia == null){
+      _instancia = Provider._internal(key: key, child: child);
+    }
+    return _instancia!;
+  }
+
+  Provider._internal({Key? key, required super.child}) : super(key: key);
+
   final loginBloc = LoginBloc();
 
-  Provider({Key? key, required super.child}) : super(key: key);
+  //Provider({Key? key, required super.child}) : super(key: key);
+
   
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
