bbafddf3fc91afb15fe858c0cab8db219767a6d6
ddienu <rockdann@hotmail.com>
2022-06-30 22:09:39 -0500

    Add: producto model page.
    Create producto model page to include de JSON data with the necessary variables to manage the app.

---
+++ lib/src/models/producto_model.dart
@@ -0,0 +1,41 @@
+// To parse this JSON data, do
+//
+//     final productoModel = productoModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ProductoModel productoModelFromJson(String str) => ProductoModel.fromJson(json.decode(str));
+
+String productoModelToJson(ProductoModel data) => json.encode(data.toJson());
+
+class ProductoModel {
+    ProductoModel({
+        required this.id,
+         this.title      = '',
+         this.valor      = 0.0,
+         this.disponible = true,
+        required this.fotoUrl,
+    });
+
+    String id;
+    String title;
+    double valor;
+    bool disponible;
+    String fotoUrl;
+
+    factory ProductoModel.fromJson(Map<String, dynamic> json) => ProductoModel(
+        id         : json["id"],
+        title      : json["title"],
+        valor      : json["valor"],
+        disponible : json["disponible"],
+        fotoUrl    : json["fotoUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id"         : id,
+        "title"      : title,
+        "valor"      : valor,
+        "disponible" : disponible,
+        "fotoUrl"    : fotoUrl,
+    };
+}
